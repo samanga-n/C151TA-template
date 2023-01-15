a823b929748c8950267921d287f9bb4601412792
Samanga Narayanan <74295785+samanga-n@users.noreply.github.com>
2023-01-15 16:39:01 +0530

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,32 @@
+AFRAME.registerComponent('name',{
+
+    schema: {
+        // data
+
+      },
+    init: function(){
+
+        // Do something when component first attached
+
+    },
+    
+    update: function(){
+
+        // Do something when component's data is updated
+
+    },
+
+    remove: function(){
+
+        // Do something when component or entity is detached
+
+    },
+
+    tick: function(){
+
+
+        // Do something on every scene tick or frame
+    }
+
+
+})
